f845bb4c6d9b7639af9d8e9c9e6d9de6df35aa41
newandthe <ksturtle2@naver.com>
2023-01-06 14:31:42 +0900

    workproject

---
+++ src/sample_01/MainClass.java
@@ -0,0 +1,14 @@
+package sample_01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.printf("%s   \n", "Hello");
+		// printf의 f는 포맷  표현자료형   값
+		System.out.printf("%d \n", 3);
+		System.out.println("Hello " + "World");
+	}
+
+}
